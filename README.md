5010e4b2128349cc57134b0e0c89ff992662372d
xyjk6666 <2895077061@qq.com>
2026-04-23 17:44:26 +0800

    新增从移动端接口抢

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-script.py：抢单个。  lecture_script.py：串行抢多个
+script.py
 
 进入对应网页后，按F12进入开发者模式，点击“网络”，清除一下请求日志
 
-点击要抢的讲座“详情”，从新增的几个请求的负载中找到WID，复制到脚本
+点击要抢的讲座“详情”，从新增的几个请求的负载中找到WID，复制到脚本。可以复制多个
 
 同样复制cookie到脚本，设置好时间，可以提前几分钟启动等待
+
+mobile_lecture.py：从移动端接口抢，需要进入网页https://ehall.seu.edu.cn/gsapp/sys/yddjzxxtjappseu/*default/index.do#/hdyy
+
+方法与前面一样
--- mobile_lecture.py
+++ mobile_lecture.py
@@ -5,24 +5,38 @@ import datetime
 import base64
 import json
 
-#配置cookie和讲座id，时间
+#配置cookie和讲座id，时间，次数
 COOKIE = ""
 WIDS = [""]
-TARGET_TIME = datetime.datetime(2026, 4, 22, 18, 59, 59,980000)
+TARGET_TIME = datetime.datetime(2026, 4, 23, 18, 59, 59,980000)
 Count=20
+
+#进入的网页
+#https://ehall.seu.edu.cn/gsapp/sys/yddjzxxtjappseu/*default/index.do#/hdyy
 
 logs=[]
 
-BASE_URL = "https://ehall.seu.edu.cn/gsapp/sys/jzxxtjapp/hdyy"
+BASE_URL = "https://ehall.seu.edu.cn/gsapp/sys/yddjzxxtjappseu/modules/hdyy"
 #CHECK_URL = f"{BASE_URL}/appiontCheck.do"
 VCODE_URL = f"{BASE_URL}/vcode.do"
-SAVE_URL = f"{BASE_URL}/yySave.do"
+SAVE_URL = f"{BASE_URL}/addReservation.do"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
     "Cookie": COOKIE,
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Accept": "application/json, text/javascript, */*; q=0.01"
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Origin": "https://ehall.seu.edu.cn",
+    "Referer": "https://ehall.seu.edu.cn/gsapp/sys/yddjzxxtjappseu/index.html",
+    "X-Requested-With": "XMLHttpRequest",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "sec-ch-ua": '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"'
 }
 
 
@@ -53,32 +67,28 @@ def auto_task_flow():
             logs.append(f"第{count}次：获取并识别验证码...")
             captcha_text = ""
             try:
-                vcode_payload = {"_": int(time.time() * 1000)}
-                vcode_resp = session.post(VCODE_URL, data=vcode_payload, timeout=6)
+                vcode_resp = session.get(VCODE_URL, params={'_': int(time.time() * 1000)}, timeout=5)
                 resp_json = vcode_resp.json()
-                if resp_json.get("success") == True:
-                    full_base64_str = resp_json.get("result")
-                    clean_base64 = full_base64_str.split(",")[1]
+                full_base64 = resp_json.get('datas', "")
+                if "base64," in full_base64:
+                    clean_base64 = full_base64.split("base64,")[1]
                     img_bytes = base64.b64decode(clean_base64)
                     captcha_text = ocr.classification(img_bytes)
-                    logs.append(f"OCR 识别结果: {captcha_text}")
                 else:
                     logs.append(f"验证码接口未返回成功状态{vcode_resp.text}")
                     continue
             except Exception as e:
                 logs.append(f"获取验证码失败: {e}")
                 continue
+            logs.append(f"验证码识别结果{captcha_text}")
             if len(captcha_text) != 4 or not captcha_text.isdigit():
                 logs.append("验证码识别明显有误，重新获取...")
                 continue
 
             #发送最终保存请求
-            inner_data = {
-                "HD_WID": wid,
-                "vcode": captcha_text
-            }
             save_payload = {
-                "paramJson": json.dumps(inner_data)
+                "wid": wid,
+                "vcode": captcha_text
             }
             final_resp=None
             try:
@@ -87,7 +97,7 @@ def auto_task_flow():
                     logs.append(f"报错{final_resp}")
                     continue
                 final_json = final_resp.json()
-                if final_json.get("code") == 200 and final_json.get("success") == True:
+                if final_json.get("code") == 0 and final_json.get("datas") == 1:
                     logs.append("服务器返回成功，预约已锁定！")
                     break
                 else:
